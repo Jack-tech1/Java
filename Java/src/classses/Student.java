b25a1f9b64f76507e05aa7aa20c90a9c0fca1e6e
Your Name <you@example.com>
2025-01-21 20:35:02 +0530

    First Commit of Class

---
+++ Java/src/classses/Student.java
@@ -0,0 +1,18 @@
+package classses;
+
+public class Student {
+	
+	int rollno;
+	String name;
+	char grade;
+	String status;
+	
+	void display()
+	{
+		System.out.println("Roll Number is: "+rollno);
+		System.out.println("Student Name Is: "+ name);
+		System.out.println("Grade is "+ grade);
+		System.out.println("Status is: "+ status);
+	}
+
+}
---
+++ Java/src/classses/StudentMain.java
@@ -0,0 +1,19 @@
+package classses;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s= new Student();
+		s.rollno=1;
+		s.name="Jack";
+		s.grade='F';
+		s.status="Pass";
+		s.display();
+		
+		
+
+	}
+
+}
